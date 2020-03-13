04cdddcd8907fe0809eb5efc3fe7962651fb507f
Stefano Carluccio <stefano.carluccio@exxcellent.de>
2020-03-13 12:46:43 +0100

    created setup script and modified setup script

---
+++ setup.bat
@@ -0,0 +1,17 @@
+@echo OFF
+echo Starting Setup
+
+echo setting up express.js-server...
+CD %~dp0client-side-include
+call npm install
+
+echo setting up react-client..
+CD %~dp0client-side-include\public\teamBlue\client-side-product
+call npm install
+
+echo setting up angular-client..
+CD %~dp0client-side-include\public\teamRed\client-side-product-details
+call npm install -g @angular/cli
+call npm install
+
+CD %~dp0
