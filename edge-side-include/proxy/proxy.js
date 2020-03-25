e82f7964bb2028de05ec1e8327e20084660cb4a8
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2020-03-25 11:35:27 +0100

    react edge side works

--- edge-side-include/proxy/proxy.js
+++ edge-side-include/proxy/proxy.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const proxy = require("express-http-proxy");
 const ESI = require("nodesi");
 const esiMiddleware = require("nodesi").middleware;
@@ -8,15 +9,23 @@ const esi = new ESI({});
 
 const app = express();
 
+const proxyUrl = "http://localhost:5000";
+const reactUrl = "http://localhost:3000";
+
 app.use(esiMiddleware());
 
-app.use("/", proxy("http://localhost:5000", {
-    userResDecorator: (proxyRes, proxyResData) => {
-        return proxyRes.headers["content-type"] === "text/html" ?
-            esi.process(proxyResData.toString()) : proxyResData;
+app.use("/", proxy(proxyUrl,
+    {
+        userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+            if (proxyRes.headers["content-type"] === "text/html") {
+                return esi.process(proxyResData.toString());
+            }
+            return proxyResData;
+        }
     }
-}));
+));
 
-app.listen(process.env.port || port);
 
-console.log("Running at Port " + port);
+app.listen(process.env.port || port, () => {
+    console.log("Running at Port " + port);
+});
--- edge-side-include/public/teamBlue/edge-side-product/pages/index.js
+++ edge-side-include/public/teamBlue/edge-side-product/pages/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Head from "next/head";
+
 import App from './App';
 
 import "./index.scss";
 
 const Index = () => (
     <div className="root">
-        <Head>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
-            <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-        </Head>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+
         <App />
     </div>
 );
