18d34e4c088bbff364ac578953477cfb48f2c373
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2020-03-30 14:28:13 +0200

    copied vue client side cart project and removed postmessage call

---
+++ edge-side-include/public/teamGreen/edge-side-cart/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <app-cart :productTuples="productTuples"></app-cart>
+  </div>
+</template>
+
+<script>
+import Cart from "./components/Cart";
+export default {
+  name: "App",
+  data: () => {
+    return {
+      productTuples: []
+    };
+  },
+  beforeCreate() {
+  },
+  components: { appCart: Cart },
+  methods: {
+    addToProductTuples(newProductTuple, body, value) {
+
+      let exists = false;
+      this.productTuples.forEach(productTuple => {
+        if (productTuple.product.id === newProductTuple.product.id) {
+          productTuple.amount += parseInt(newProductTuple.amount);
+          exists = true;
+        }
+      });
+
+      if (exists === false) {
+        this.productTuples.push({
+          product: body,
+          amount: parseInt(value)
+        });
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+body::-webkit-scrollbar {
+  display: none;
+
+  width: 2px;
+  height: 2px;
+}
+body::-webkit-scrollbar-track {
+  background-color: teal;
+}
+body::-webkit-scrollbar-thumb {
+  background-color: white;
+}
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
